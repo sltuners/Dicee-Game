8048aed49657eef3965cb39d317c5d6612b85787
SAS <56768310+sltuners@users.noreply.github.com>
2022-02-06 16:56:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var imgP1 = "images/dice" + randomNumber1 + ".png";
-var diceP1 = (document.querySelector(".img1").src = imgP1);
-console.log(diceP1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+var imgP1 = "images/dice" + randomNumber1 + ".png"; // concatenate the link
+var diceP1 = document.querySelector(".img1").src = imgP1; // change SRC attribute
+// console.log(diceP1); // debug it
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var imgP2 = "images/dice" + randomNumber2 + ".png";
-var diceP2 = (document.querySelector(".img2").src = imgP2);
-console.log(diceP2);
+var diceP2 = document.querySelector(".img2").src = imgP2;
+// console.log(diceP2);
 var header;
 
+// changing h1 by SWITCH
 // switch (a = randomNumber1 - randomNumber2) {
 //   case (>0):
 //     header = "P1 wins it all";
@@ -23,8 +24,9 @@ var header;
 // }
 // console.log(a);
 
-if (randomNumber1 > randomNumber2) header = "Make it asap";
-else if (randomNumber1 < randomNumber2) header = "Bad idea";
+// changing h1 by IF
+if (randomNumber1 > randomNumber2) header = "Make it!";
+else if (randomNumber1 < randomNumber2) header = "Bad idea.";
 else header = "Not shure, refresh me";
 document.querySelector("h1").textContent = header;
-console.log(header);
+
